3c08a743ea3a2a6a11c63ae70e6c7e55f82dc957
Sophie McDonald <sophiermcdonald@gmail.com>
2017-11-29 17:59:00 +1100

    create method to remove sun and readd it to the front of the array

--- Sophie/wk01/3-wed/array/array.js
+++ Sophie/wk01/3-wed/array/array.js
@@ -1,1 +1,13 @@
-var days_of_the_week = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var daysOfTheWeek = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+daysOfTheWeek.pop();
+console.log(daysOfTheWeek)
+daysOfTheWeek.unshift('Sunday');
+console.log(daysOfTheWeek);
+
+var newDaysOfTheWeek = [
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  ['Saturday', 'Sunday']
+];
+newDaysOfTheWeek.splice([1]);
+console.log(newDaysOfTheWeek);
