497dd8501233af4c8c97c96c531260938ecee898
Sophie McDonald <sophiermcdonald@gmail.com>
2017-11-30 20:27:03 +1100

    Working on solution

--- Sophie/wk01/4-thu/publictransport/publictransport.js
+++ Sophie/wk01/4-thu/publictransport/publictransport.js
@@ -1,5 +1,20 @@
 console.log("publictransport");
 
-var alamein = ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie'];
-var glenWaverly = ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga'];
-var sandringham = ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran','Windsor'];
+
+// var glenWaverly = ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga'];
+// var sandringham = ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran','Windsor'];
+var station = ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie'];
+var origin = station.indexOf('Richmond');
+
+//prompt('where does your journey start?');
+// indexOf(origin);
+var destination = [station.indexOf('Burnley')];
+var one = destination.toString();
+var route = station.slice(0, 3);
+//prompt('where does your journey end?');
+console.log(route);
+
+//index of origin
+//index of destination
+//slice origin to destination
+//convert to strings
