d5950309e862c71b994302c4aac90d7a8b4b0ee0
Sophie McDonald <sophiermcdonald@gmail.com>
2017-11-30 15:57:22 +1100

    Add 3 arrays with train line stops

---
+++ Sophie/wk01/4-thu/publictransport/publictransport.js
@@ -0,0 +1,5 @@
+console.log("publictransport");
+
+var alamein = ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie'];
+var glenWaverly = ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga'];
+var sandringham = ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran','Windsor'];
