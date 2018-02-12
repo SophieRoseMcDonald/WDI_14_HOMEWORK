223d9546e8cbff41b0b656d4cb19c3875cd33178
Sophie McDonald <sophiermcdonald@gmail.com>
2018-02-12 18:22:04 +1100

    Mon wk10 phone_test homework

---
+++ Sophie/wk10/1-mon/phone/phone.rb
@@ -0,0 +1,27 @@
+class Phone
+
+
+  def initialize(number)
+   @number = number.gsub(/[()-.\s]/, "")
+  end
+
+  def number
+    if @number.length == 11 && @number[0] == '1'
+      @number = @number.slice(1, 11)
+    elsif
+      @number.length == 11 || @number.length == 9
+      return '0000000000'
+    end
+      return @number
+    end
+
+    def area_code
+      @number[0..2]
+    end
+
+    def to_s
+      "(#{area_code}) #{@number[3..5]}-#{@number[6..9]}"
+    end
+
+
+end
