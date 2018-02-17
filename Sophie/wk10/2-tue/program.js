fb6cd7bd980727c443938e49db32e9a04c7db18e
Sophie McDonald <sophiermcdonald@gmail.com>
2018-02-17 12:35:29 +1100

    homework

---
+++ Sophie/wk10/2-tue/program.js
@@ -0,0 +1,10 @@
+// var a = 1;
+// var b = 2;
+//
+// console.log((a + b));
+var sum = function() {
+  var num1 = prompt('enter a number')
+  var num2 = prompt('enter another number')
+  console.log(num1 + num2)
+}
+ sum()
